956eb5a8b7a3dd1d2dd1821a17b71509b4a10b1b
akshat1032 <akshat.30oct@gmail.com>
2020-10-14 23:27:53 +0530

    Added : Refactor and Ability to find cheapest hotel based on date added

--- src/com/capgemini/hotelreservation/AddHotelNameAndRateTest.java
+++ src/com/capgemini/hotelreservation/AddHotelNameAndRateTest.java
@@ -10,30 +10,41 @@ import com.capgemini.hotelreservation.HotelReservation.CustomerType;
 public class AddHotelNameAndRateTest {
 
 	private HotelReservation hotelReservation = new HotelReservation();
+	String hotelName1 = "Lakewood";
+	int rate1 = 110;
+	String hotelName2 = "Bridgewood";
+	int rate2 = 160;
+	String hotelName3 = "Ridgewood";
+	int rate3 = 220;
 
 	// Test to check for addition of hotel to the reservation system
 	@Test
 	public void testAddHotelNameAndRate() {
-		String hotelName1 = "Lakewood";
-		int rate1 = 110;
-		String hotelName2 = "Bridgewood";
-		int rate2 = 160;
-		String hotelName3 = "Ridgewood";
-		int rate3 = 220;
+		
 		// Calling the function to store details
 		hotelReservation.addHotelNameAndRate(rate1, hotelName1, CustomerType.RegularCustomer);
 		hotelReservation.addHotelNameAndRate(rate2, hotelName2, CustomerType.RegularCustomer);
 		hotelReservation.addHotelNameAndRate(rate3, hotelName3, CustomerType.RegularCustomer);
-
-		// Storing returned details in array list by hotel
-		ArrayList<Hotel> hotelDetails1 = HotelReservation.hotelList.get(hotelName1);
-		ArrayList<Hotel> hotelDetails2 = HotelReservation.hotelList.get(hotelName2);
-		ArrayList<Hotel> hotelDetails3 = HotelReservation.hotelList.get(hotelName3);
-
+		
 		// Testing with rate
-		Assert.assertEquals(110, hotelDetails1.get(0).getRate());
-		Assert.assertEquals(160, hotelDetails2.get(0).getRate());
-		Assert.assertEquals(220, hotelDetails3.get(0).getRate());
+		Assert.assertEquals(110, HotelReservation.hotelList.get("Lakewood").getRate());
+		Assert.assertEquals(160, HotelReservation.hotelList.get("Bridgewood").getRate());
+		Assert.assertEquals(220, HotelReservation.hotelList.get("Ridgewood").getRate());
 
+	}
+	
+	// Test to check for cheapest hotel for date range
+	@Test
+	public void testCheapestHotelForDate() {
+		
+		// Calling the function to store details
+		hotelReservation.addHotelNameAndRate(rate1, hotelName1, CustomerType.RegularCustomer);
+		hotelReservation.addHotelNameAndRate(rate2, hotelName2, CustomerType.RegularCustomer);
+		hotelReservation.addHotelNameAndRate(rate3, hotelName3, CustomerType.RegularCustomer);
+		//Giving Date
+		ArrayList<String> date = new ArrayList<String>();
+		date.add("10September2020");
+		date.add("11September2020");
+		Assert.assertEquals("Lakewood, Total Rates: $220", hotelReservation.cheapestHotelByDate(date));
 	}
 }
--- src/com/capgemini/hotelreservation/HotelReservation.java
+++ src/com/capgemini/hotelreservation/HotelReservation.java
@@ -1,7 +1,12 @@
 package com.capgemini.hotelreservation;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.*;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HotelReservation {
 
@@ -18,16 +23,47 @@ public class HotelReservation {
 	}
 
 	// Instance variable for storing details of hotel and list of hotels
-	public static ArrayList<Hotel> hotelDetails = new ArrayList<Hotel>();
-	public static HashMap<String, ArrayList<Hotel>> hotelList = new HashMap<String, ArrayList<Hotel>>();
+	public static HashMap<String, Hotel> hotelList = new HashMap<String, Hotel>();
 
 	// Adding hotel name and rate for each hotel to the system
 	public void addHotelNameAndRate(int rate, String hotelName, CustomerType customerType) {
-		hotelDetails.add(new Hotel(rate, customerType));
+		Hotel hotelDetails = new Hotel(rate, customerType);
 		hotelList.put(hotelName, hotelDetails);
+	}
 
-		// Initializing an empty array list to add details of the next hotel
-		hotelDetails = new ArrayList<>();
+	// Return cheapest hotel by range of date
+	public String cheapestHotelByDate(ArrayList<String> date) {
+		LocalDate dateFormat[] = new LocalDate[2]; int dateIndex = 0;
+		String dateregex = "^(([0-9])|([0-2][0-9])|([3][0-1]))(January|February|March|April|May|June|July|August|September|October|November|December)\\d{4}$";
+		Pattern pattern = Pattern.compile(dateregex);
+		for (String inputDate : date) {
+			Matcher m = pattern.matcher(inputDate);
+			if (m.matches()) {
+				dateFormat[dateIndex] = LocalDate.of((Integer.parseInt(inputDate.substring(inputDate.length() - 4))),
+						Month.valueOf(inputDate.toUpperCase().substring(2, inputDate.length() - 4)),
+						Integer.parseInt(inputDate.substring(0, 2)));
+				dateIndex++;
+			}
+		}
+		int rate[] = { hotelList.get("Lakewood").getRate(), hotelList.get("Bridgewood").getRate(),
+				hotelList.get("Ridgewood").getRate() };
+		int totalRate[] = new int[3];
+		int hotelIndex = 0;
+		int noOfDays = dateFormat[1].getDayOfMonth() - dateFormat[0].getDayOfMonth();
+		int counterNoOfDays = 0;
+		do {
+			do {
+				totalRate[hotelIndex] = totalRate[hotelIndex] + rate[hotelIndex];
+				counterNoOfDays++;
+			}while (counterNoOfDays <= noOfDays);
+			hotelIndex++;
+			counterNoOfDays = 0;
+		} while (hotelIndex != 3);
+		return totalRate[0] < totalRate[1]
+				? totalRate[0] < totalRate[2] ? "Lakewood, Total Rates: $" + totalRate[0]
+						: "Ridgewood, Total Rates: $" + totalRate[2]
+				: totalRate[1] < totalRate[2] ? "Bridgewood, Total Rates: $" + totalRate[1]
+						: "Ridgewood, Total Rates: $" + totalRate[2];
 	}
 
 	public static void main(String[] args) {
