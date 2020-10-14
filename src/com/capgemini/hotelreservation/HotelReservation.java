227ff021e1a6e05de57c067a9e1f78127c6c09fc
akshat1032 <akshat.30oct@gmail.com>
2020-10-14 18:51:26 +0530

    Added : Welcome message added

---
+++ src/com/capgemini/hotelreservation/HotelReservation.java
@@ -0,0 +1,12 @@
+package com.capgemini.hotelreservation;
+
+import java.util.logging.Logger;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+
+		Logger logger = Logger.getLogger(HotelReservation.class.getName());
+		//Welcome message added
+		logger.info("Welcome to Hotel Reservation Program");
+	}
+}
